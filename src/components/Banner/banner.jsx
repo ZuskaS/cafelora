713f94ecdb713059337b0efb6bb364102884f045
Zuzana Ilečková <yes>
2025-05-03 11:10:26 +0200

    přidání id na sekce pro vytvoření odkazu v navigaci. Id přidáno do banner, contact, gallery, menu.

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -2,7 +2,7 @@ import './banner.css';
 
 export const Banner = () => {
   return (
-    <section className="banner">
+    <section className="banner" id="home">
       <div className="container">
         <div className="banner__content">
           <h1>Přijď to ochutnat!</h1>
